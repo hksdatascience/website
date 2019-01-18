809f5017a959d8903840d09ffb92b9f0c6d824a1
ericenglin <ericenglin@gmail.com>
2019-01-17 23:51:40 -0500

    d3 hopefully debugged, adding in GM and Google stocks

--- js/GOOGL.js
+++ js/GOOGL.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 50, bottom: 30, left: 50},
-    width = 700 - margin.left - margin.right,
+    width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y-%m-%d").parse,
@@ -31,7 +31,7 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("../data/Tesla_Stock.csv", function(data) {
+d3.csv("../data/GM_Stock.csv", function(data) {
 
   data.forEach(function(d) {
     d.date = parseDate(d.date);
@@ -45,12 +45,11 @@ d3.csv("../data/Tesla_Stock.csv", function(data) {
   x.domain([data[0].date, data[data.length - 1].date]);
   y.domain(d3.extent(data, function(d) { return d.close; }));
 
-  //This code creates the x axis
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-  //This code creates the y axis
+
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -61,26 +60,21 @@ d3.csv("../data/Tesla_Stock.csv", function(data) {
       .style("text-anchor", "end")
       .text("Price ($)");
 
-  //This code creates the actual line
   svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", line);
 
-  //This code creates the mouseover display
   var focus = svg.append("g")
       .attr("class", "focus")
       .style("display", "none");
 
-  //This code creates the little circle
   focus.append("circle")
       .attr("r", 4.5);
 
-  //This code creates the text that goes outside the circle
   focus.append("text")
       .attr("x", 12)
       .attr("dy", ".35em");
-
 
   svg.append("rect")
       .attr("class", "overlay")
@@ -99,11 +93,4 @@ d3.csv("../data/Tesla_Stock.csv", function(data) {
     focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
     focus.select("text").text(formatCurrency(d.close));
   }
-
-  svg.append("text")
-        .attr("x", 550)             
-        .attr("y", 400)
-        .attr("text-anchor", "middle")  
-        .style("font-size", "30px") 
-        .text("Tesla");
 });
